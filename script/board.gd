b3fbfaeb58220d93079bc8dc24b98ca34f2114b1
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-02-17 20:56:42 +0800

    feat:set row and col variable

--- script/board.gd
+++ script/board.gd
@@ -4,8 +4,10 @@ extends GridContainer
 func _ready() -> void:
 	var cell :PackedScene = load("res://scene/cell.tscn")
 	if cell is PackedScene and cell.can_instantiate():
-		for i in range(1,10):
-			var cell_node:Button = cell.instantiate()
+		for i in range(0,9):
+			var cell_node:Cell = cell.instantiate()
+			cell_node.col = i%3+1;
+			cell_node.row = i/3 + 1;
 			add_child(cell_node)
 
 
--- script/cell.gd
+++ script/cell.gd
@@ -1,5 +1,11 @@
+class_name Cell
 extends Button
 
+
+@export
+var row:int
+@export 
+var col:int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +17,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-
-func _on_pressed() -> void:
-	print("_on_pressed")
+func _pressed() -> void:
+	prints("_pressed",row,col)
