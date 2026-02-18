0a349ec2cbda7a68821091f47413d2339fb7dd66
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-02-18 15:21:03 +0800

    feat: set size of font

--- script/sign.gd
+++ script/sign.gd
@@ -3,7 +3,8 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	label_settings = LabelSettings.new()
+	label_settings.font_size = int(get_viewport_rect().size.y/10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,7 +15,7 @@ func _process(delta: float) -> void:
 func _on_board_on_side(current_side: GameEnum.BoardSide) -> void:
 	if current_side == GameEnum.BoardSide.RED :
 		text = "red side"
-		add_theme_color_override("font_color",Color(1,0,0,0.6))
+		label_settings.font_color = Color(1,0,0,0.6)
 	else:
 		text = "blue side"
-		add_theme_color_override("font_color",Color(0,0,1,0.6))
+		label_settings.font_color = Color(0,0,1,0.6)
