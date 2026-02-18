6a7b04595cc4b4883c6ad86c1ee81172a6f4f239
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-02-18 19:14:56 +0800

    feat: setup a font scale

---
+++ script/font_scale.gd
@@ -0,0 +1,19 @@
+class_name FontScale
+extends Node
+
+@export var base_theme: Theme = preload("res://theme/base_theme.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	get_viewport().size_changed.connect(setup_font_size)
+	setup_font_size()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func setup_font_size():
+	base_theme.default_font_size = get_viewport().get_visible_rect().size.y/20
