2a38a93ae03f9071766e6380713f27a6c98fa002
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-02-19 11:31:05 +0800

    fix: using font in tic-tac-toe

--- games/tic-tac-toe/script/sign.gd
+++ games/tic-tac-toe/script/sign.gd
@@ -1,10 +1,9 @@
-extends ResizeLabel
+extends Label
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if not label_settings:
-		label_settings = LabelSettings.new()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,19 +14,19 @@ func _process(delta: float) -> void:
 func _on_board_on_side(current_side: GameEnum.BoardSide) -> void:
 	if current_side == GameEnum.BoardSide.RED :
 		text = "red side"
-		label_settings.font_color = Color(1,0,0,0.6)
+		add_theme_color_override("font_color",Color(1,0,0,0.6))
 	else:
 		text = "blue side"
-		label_settings.font_color = Color(0,0,1,0.6)
+		add_theme_color_override("font_color",Color(0,0,1,0.6))
 
 
 func _on_board_on_finish(winner: GameEnum.BoardSide) -> void:
 	if winner == GameEnum.BoardSide.RED :
 		text = "red win"
-		label_settings.font_color = Color(1,0,0,0.6)
+		add_theme_color_override("font_color",Color(1,0,0,0.6))
 	elif winner == GameEnum.BoardSide.BLUE:
 		text = "blue win"
-		label_settings.font_color = Color(0,0,1,0.6)
+		add_theme_color_override("font_color",Color(0,0,1,0.6))
 	else:
 		text = "draw"
-		label_settings.font_color = Color(1,1,1,0.6)
+		add_theme_color_override("font_color",Color(1,1,1,0.6))
