b5c114c5f771a9bb033cd0a753a4aae66ff835f6
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-02-21 11:47:29 +0800

    feat: add function generate_lines to genrate line

--- games/tic-tac-toe/script/board.gd
+++ games/tic-tac-toe/script/board.gd
@@ -15,6 +15,9 @@ var n_row: int
 @export
 var n_col: int
 
+@export
+var n_win: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	columns = n_col
@@ -43,22 +46,74 @@ func process_cell_trugger(id:int):
 	on_side.emit.call_deferred(side)
 	check_board(id)
 
+func generate_lines(i:int ,j: int):
+	var result : Array[Array] = []
+	for k in range(1-n_win,n_win):
+		var fit:bool = true
+		var line:Array[int] = []
+		for a in range(0,n_win):
+			var new_i = i - k + a
+			if new_i < 0 or new_i >= n_row:
+				fit = false
+				break
+			line.append(new_i * n_col + j)
+		if fit:
+			result.append(line)
+		fit = true
+		line = []
+		for a in range(0,n_win):
+			var new_j = j - k + a
+			if new_j < 0 or new_j >= n_col:
+				fit = false
+				break
+			line.append(i * n_col + new_j)
+		if fit:
+			result.append(line)
+		fit = true
+		line = []
+		for a in range(0,n_win):
+			var new_i = i - k + a
+			var new_j = j - k + a
+			if new_i < 0 or new_i >= n_row or new_j < 0 or new_j >= n_col:
+				fit = false
+				break
+			line.append(new_i * n_col + new_j)
+		if fit:
+			result.append(line)
+		fit = true
+		line = []
+		for a in range(0,n_win):
+			var new_i = i - k + a
+			var new_j = j + k - a
+			if new_i < 0 or new_i >= n_row or new_j < 0 or new_j >= n_col:
+				fit = false
+				break
+			line.append(new_i * n_col + new_j)
+		if fit:
+			result.append(line)
+	return result
+
 func check_board(id: int):
-	var lines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-	var check_line= []
-	for line in lines:
-		if id in line:
-			check_line.append(line)
+	var i = id / n_col
+	var j = id % n_col
+	prints("i",i,"j",j)
+	var check_line= generate_lines(i,j)
+	prints(check_line)
 	var base_side = cells[id].side
 	for line in check_line:
-		if cells[line[0]].side == base_side and cells[line[1]].side == base_side and cells[line[2]].side == base_side:
+		var fit:bool = true
+		for index in line:
+			if cells[index].side != base_side:
+				fit = false
+				break
+		if fit:
 			emit_signal("on_finish",base_side)
-			for cell in cells:
-				cell.disabled = true
+			for i_cell in cells:
+				i_cell.disabled = true
 			return
 	var full = true
-	for cell in cells:
-		if not cell.disabled:
+	for i_cell in cells:
+		if not i_cell.disabled:
 			full = false
 	if full:
 		emit_signal("on_finish",-1)
