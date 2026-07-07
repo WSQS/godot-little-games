6cb7afeec98f7af3f3f3be4892cecc6f7e6662db
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-07-07 15:49:36 +0800

    fix: remove unused _ready function from vehicle.gd

--- games/stray/script/vehicle.gd
+++ games/stray/script/vehicle.gd
@@ -3,10 +3,6 @@ extends Node2D
 var r := 8.0
 var alpha := .0
 var v := Vector2()
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
